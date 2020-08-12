ce9f750354ed8f82967f5327997a7095a73581c6
付 英 <fylalala@outlook.com>
2020-08-12 19:11:35 +0800

    H_index and PascalTriangleII

---
+++ AugustDay4_ PowerofFour/AugustDay4_ PowerofFour.cpp
@@ -0,0 +1,60 @@
+﻿// AugustDay4_ PowerofFour.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+using namespace std;
+
+bool isPowerOfFour(int num) {
+    if (num <= 0)
+        return false;
+    if (num == 1)
+        return true;
+    if (num % 4 != 0)
+        return false;
+    else
+    {
+        while (num)
+        {
+            num = num / 4;
+            if (num>1&&num % 4 != 0)
+            {
+                return false;
+            }
+            else
+            {
+                if (num == 1)
+                    return true;
+                else
+                    continue;
+                /*else if (num >= 4)
+                    continue;
+                else
+                    return false;*/
+            }
+            
+        }
+    }
+    if (num == 1)
+        return true;
+    else
+        return false;
+}
+int main()
+{
+    int num = 100;
+    bool result = isPowerOfFour(num);
+    cout << result << endl;
+    //std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ PascalTriangleII/PascalTriangleII.cpp
@@ -0,0 +1,46 @@
+﻿// PascalTriangleII.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<int> getRow(int rowIndex) {
+    vector<vector<int> >arr(rowIndex+1);
+    for (int i = 0; i <= rowIndex; i++)
+    {
+        arr[i].resize(rowIndex + 1);
+    }
+    arr[0][0] = 1;
+    for (int i = 1; i < rowIndex + 1; i++)
+    {
+        arr[i][0] = arr[i][rowIndex] = 1;
+        for (int j = 1; j < rowIndex + 1; j++)
+        {
+            arr[i][j]=arr[i - 1][j - 1] + arr[i - 1][j];
+        }
+    }
+    return arr[rowIndex];
+}
+int main()
+{
+    int rowIndex = 4;
+    //cout << rowIndex << endl;
+    vector<int> arr = getRow(rowIndex);
+    for (int i = 0; i < rowIndex + 1; i++)
+    {
+        cout << arr[i]<<"\t" ;
+    }
+    //std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
